298aa5b551dc19aa7fdf55c21447777e110abe2f
Murugan.R <Murugan.R@EXIND3020-11.allfon.in>
2022-03-21 13:00:01 +0530

    updated

--- Jenkins-spring-HandsOn/src/main/java/com/example/demo/controller/JenkinsController.java
+++ Jenkins-spring-HandsOn/src/main/java/com/example/demo/controller/JenkinsController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,15 @@ public class JenkinsController {
 	public String getMessage() {
 		
 		return "Ram,Wilson,Willy";
+	}
+	@GetMapping(path="lists/{dept}")
+	public String getStudentsByDept(@PathVariable("dept") String dept) {
+		
+		List<String> list=Arrays.asList("Arun", "Khabib", "Conor");
+		
+		if(dept.equals("ece")) {
+			list = Arrays.asList("Praveen", "Khabib", "Conor");
+		}
+		return list.toString();
 	}
 }
